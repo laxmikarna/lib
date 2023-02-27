335e853bc8866c1461f90a98bf4a5d8e68a72944
laxmikarna <124425009+laxmikarna@users.noreply.github.com>
2023-02-27 17:47:27 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,5 +10,9 @@ def newDeploy(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def newGit1(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
 
   
